ee4d5678f2d74effe8087e3c3ad8b944b140d7d6
Abdullahi Salaudeen <abdullahisalaudeen65@gmail.com>
2023-07-16 18:13:27 +0100

    implemented post comment

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 // ------------Selcting DOM Element---------------
 const mainContainer = document.querySelector('.content');
 const replyBtns = document.querySelectorAll('.reply');
+const sendBtn = document.getElementById('send');
+const commentTxt = document.getElementById('sendMsg');
 
 
 // ----------------All Functions------------------
@@ -12,123 +14,76 @@ const replyBtns = document.querySelectorAll('.reply');
 
 // Load page func
 const pageLoads = ()=>{
-    fetch('./data.json')
-    .then(res => {
-        return res.json()
-    })
-    .then(data =>{
-        mainContainer.innerHTML = `<div class="msg-content">
-        <div class="comment-sec" id="1" data-id="1">
-            <div class="text-sec">
-                <div class="profile">
-                    <div class="comment-pic"><img src="./images/avatars/image-amyrobson.png" alt="?"></div>
-                    <p class="user-name">Theonlyabdull</p>
-                    <p class="date-posted">1 month ago</p>
-                    <div class="reply"><img src="./images/icon-reply.svg" alt="reply"><span class="reply-text">Reply</span></div>
-                </div>
-                <div class="comment-text-div">
-                    <p class="comment-text"> 
-                        Impressive! Though it seems the drag feature could be improved. 
-                        But overall it looks incredible. You've nailed the design and the
-                        responsiveness at various breakpoints works really well.
-                    </p>
-                </div>
-            </div>
-            <div class="rating-sec">
-                <div class="plus"><img src="./images/icon-plus.svg" alt="+"></div>
-                <p class="rating">14</p>
-                <div class="minus"><img src="./images/icon-minus.svg" alt="-"></div>
-            </div>
-        </div>
-    </div>
-
-    <!-- COMMENT-2 -->
-    <div class="msg-content">
-        <div class="comment-sec" id="2" data-flow="2">
-            <div class="text-sec">
-                <div class="profile">
-                    <div class="comment-pic"><img src="./images/avatars/image-amyrobson.png" alt="?"></div>
-                    <p class="user-name">Theonlyabdull</p>
-                    <p class="date-posted">1 month ago</p>
-                    <div class="reply"><img src="./images/icon-reply.svg" alt="reply"><span class="reply-text">Reply</span></div>
-                </div>
-                <div class="comment-text-div">
-                    <p class="comment-text"> 
-                        Impressive! Though it seems the drag feature could be improved. 
-                        But overall it looks incredible. You've nailed the design and the
-                        responsiveness at various breakpoints works really well.
-                    </p>
-                </div>
-            </div>
-            <div class="rating-sec">
-                <div class="plus"><img src="./images/icon-plus.svg" alt="+"></div>
-                <p class="rating">14</p>
-                <div class="minus"><img src="./images/icon-minus.svg" alt="-"></div>
-            </div>
-        </div>
-    </div>
-
-    <!-- POSTED REPLY -->
-    <div class="posted-reply-container">
-        <div class="msg-content posted-reply">
-            <div class="comment-sec">
-                <div class="text-sec">
-                    <div class="profile">
-                        <div class="comment-pic"><img src="./images/avatars/image-amyrobson.png" alt="?"></div>
-                        <p class="user-name">Theonlyabdull</p>
-                        <p class="date-posted">1 month ago</p>
-                        <div class="reply"><img src="./images/icon-reply.svg" alt="reply"><span class="reply-text">Reply</span></div>
-                    </div>
-                    <div class="comment-text-div">
-                        <p class="comment-text"> 
-                            Impressive! Though it seems the drag feature could be improved. 
-                            But overall it looks incredible. You've nailed the design and the
-                            responsiveness at various breakpoints works really well.
-                        </p>
-                    </div>
-                </div>
-                <div class="rating-sec">
-                    <div class="plus"><img src="./images/icon-plus.svg" alt="+"></div>
-                    <p class="rating">14</p>
-                    <div class="minus"><img src="./images/icon-minus.svg" alt="-"></div>
-                </div>
-            </div>
-        </div>
-    </div><div class="reply-content post">
-    <div class="reply-text">
-        <textarea class="post-txt-area" name="" id="" placeholder="Add a comment..."></textarea>
-    </div>
-    <div class="reply-pic">
-        <img src="./images/avatars/image-maxblagun.png" alt="?">
-    </div>
-    <div class="reply-btn-container">
-        <button class="reply-btn">SEND</button>
-    </div>
-</div>`
-        
-    })
-    .catch(err => console.error(err));
+    //display comment
+    const value = JSON.parse(localStorage.getItem('value'))
+    if (value) {
+        console.log(value);
+    }
+    
 }
 
-const clickedReply = (e)=>{
-    console.log(`fuck it worked`);
-    const replyMainParent = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-    const replyParent = e.target.parentNode.parentNode.parentNode.parentNode;
-    console.log(replyMainParent);
-    console.log(replyParent);
-    const replyMsgBox = `<div class="reply-content post">
-    <div class="reply-text">
-        <textarea class="post-txt-area" name="" id="" placeholder="Add a comment..."></textarea>
-    </div>
-    <div class="reply-pic">
-        <img src="./images/avatars/image-maxblagun.png" alt="?">
-    </div>
-    <div class="reply-btn-container">
-        <button class="reply-btn">SEND</button>
-    </div>
-</div>`;
-    replyMainParent.insertBefore(replyMsgBox, replyParent.nextSibling);
-//insert the div under the reply parent
+const clickedSend = ()=>{
+    const commentValue = sendMsg.value;
+    // console.log(sendMsg.value);
+
+    //if a value is inputed
+    if (commentValue) {
+
+        //save comment to local storage
+        localStorage.setItem('value', JSON.stringify(commentValue));
+        console.log(`fuck it worked`); 
+
+        
+        //getting user details from local storage
+        const userName = 'Abdullah';
+        // const userPic = localStorage.getItem(JSON.parse('userStoredPic'));
+        
+        //creating new msg div and add class
+        const msgContentDiv = document.createElement('div');
+        msgContentDiv.classList.add('msg-content');
+
+        //inserted all element and content
+            msgContentDiv.innerHTML = `<div class="comment-sec">
+            <div class="text-sec">
+                <div class="profile">
+                    <div class="comment-pic"><img src="./images/avatars/image-maxblagun.png" alt="?"></div>
+                    <p class="user-name">${userName}</p>
+                    <p class="user-tag">you</p>
+                    <p class="date-posted">1 month ago</p>
+                    <div class="del-ed">
+                        <div class="delete"><img src="./images/icon-delete.svg" alt="reply"><span class="reply-text">Delete</span></div>
+                        <div class="edit"><img src="./images/icon-edit.svg" alt="reply"><span class="reply-text">Edit</span></div>
+                    </div>
+                </div>
+                <div class="comment-text-div">
+                    <p class="comment-text"> 
+                        ${commentValue}
+                    </p>
+                </div>
+            </div>
+            <div class="rating-sec">
+                <div class="plus"><img src="./images/icon-plus.svg" alt="+"></div>
+                <p class="rating">14</p>
+                <div class="minus"><img src="./images/icon-minus.svg" alt="-"></div>
+            </div>
+        </div>`
+        mainContainer.appendChild(msgContentDiv);
+
+        //when deleted btn is clicked
+        const deleteBtn = document.querySelector('.delete');
+        deleteBtn.addEventListener('click', (e)=>{
+            const elementToRemove = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+            elementToRemove.remove();
+            // localStorage.removeItem('value')
+        })
+
+
+        //clear the input
+        commentTxt.value = '';
+    }else{
+        console.log(`input a value`);
+    }
+    
 }
 
 
@@ -138,10 +93,8 @@ const clickedReply = (e)=>{
 // when page load
 // window.addEventListener('load', pageLoads);
 
-// When reply button is clicked
-replyBtns.forEach((replyBtn)=>{
-    replyBtn.addEventListener('click', clickedReply)
-});
+// When send button is clicked
+sendBtn.addEventListener('click', clickedSend)
 
 
 
